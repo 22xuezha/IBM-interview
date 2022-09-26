8c3baf3cd0cfeda329103a7de2e7f0645eab3cc3
22xuezha <114323911+22xuezha@users.noreply.github.com>
2022-09-26 18:50:28 +0800

    Add files via upload

---
+++ CMakeProject1.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	string nameOfCourse;
+	GradeBook myGradeBook;
+
+	cout << "Initial course name is:" << myGradeBook.getCourseName() << endl;
+
+	cout << "\nPlease enter the course name:" << endl;
+	cin >> nameOfCourse;
+	myGradeBook.setCourseName(nameOfCourse);
+
+	cout << "\n";
+	myGradeBook.displayMessage();
+	
+}
---
+++ CMakeProject1.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include <iostream>
+#include<string.h>
+
+using namespace std;
+
+class GradeBook
+{
+public:
+	void setCourseName(string name)
+	{
+		courseName = name;
+	}
+	string getCourseName() const
+	{
+		return courseName;
+	}
+	void displayMessage() const
+	{
+		cout << "Welcome to the grade book for\n" << getCourseName() << "!" << endl;
+	}
+private:
+	string courseName;
+};
+
+
+
+
